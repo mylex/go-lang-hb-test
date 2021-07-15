4c1aea6fe9f25378d13b1e4d7e0784197478780d
Abu Sufian <asufian47@gmail.com>
2021-07-16 02:49:10 +0900

    Set Api rate Limit

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
+	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -6,11 +6,25 @@ import (
 	"net/http"
 )
 
+var limiter = NewIPRateLimiter(1, 5)
+
 func Init(port int) {
 	portPrefix := fmt.Sprintf(":%d", port)
-	err := http.ListenAndServe(portPrefix, Router())
+	err := http.ListenAndServe(portPrefix, limitMiddleware(Router()))
 
 	if err != nil {
 		log.Fatalf("unable to start server: %s", err.Error())
 	}
+}
+
+func limitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		limiter := limiter.GetLimiter(r.RemoteAddr)
+		if !limiter.Allow() {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
